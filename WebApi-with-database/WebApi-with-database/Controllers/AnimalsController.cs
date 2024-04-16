56c85d9b88411e67fbe40fb2fc758c149e81d592
Mateusz Skolozynski <mateusz.skolozynski15@gmail.com>
2024-04-16 12:42:08 +0200

    Initialize project

---
+++ WebApi-with-database/WebApi-with-database/Controllers/AnimalsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_with_database.Controllers;
+
+[ApiController]
+// [Route("/api/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok();
+    }
+    
+}
